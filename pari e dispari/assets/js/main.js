1e1949e1ff04cfae6812cb322d67410e3be5f0af
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-27 16:10:42 +0100

    genera numero casuale

--- pari e dispari/assets/js/main.js
+++ pari e dispari/assets/js/main.js
@@ -1,1 +1,7 @@
-console.log('workssss');
+// generiamo un numero casuale da 1 a 5
+
+function generaNumeroCasuale() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+console.log("Il numero casuale generato è: " + numeroCasuale);
