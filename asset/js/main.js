10eaaaf9e388665cf0961142d4fe6b9e598f0f26
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-27 16:01:58 +0100

    palindroma done

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,25 +1,24 @@
-// chiedere all'utente di inserire una parola
-
-const parola = prompt ('inserisci una parola')
-
-console.log(parola);
-
-// verifico sel aparola è pal
+// Funzione per verificare se una parola è palindroma
 function isPalindrome(parola) {
     // Step 1. Usa il metodo split() per restituire un nuovo array
-    var splitString = parola.split(""); // var splitString = "hello".split("");
-    // ["h", "e", "l", "l", "o"]
+    var splitString = parola.split("");
  
     // Step 2. Usa il metodo reverse() per invertire l'array appena creato
-    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
-    // ["o", "l", "l", "e", "h"]
+    var reverseArray = splitString.reverse();
  
     // Step 3. Usa il metodo join() per unire tutti gli elementi della stringa in un array
-    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
-    // "olleh"
+    var joinArray = reverseArray.join("");
     
-    //Step 4. Restituisci la stringa invertita
-    return parola === joinArray; // "olleh"
+    // Step 4. Confronta la parola originale con la parola invertita e restituisci true se sono uguali, altrimenti false
+    return parola === joinArray;
 }
- 
-reverseString("hello");
+
+// Chiedi all'utente di inserire una parola
+const parolaUtente = prompt('Inserisci una parola:');
+
+// Verifica se la parola inserita dall'utente è palindroma
+if (isPalindrome(parolaUtente)) {
+    console.log(parolaUtente + " è una parola palindroma.");
+} else {
+    console.log(parolaUtente + " non è una parola palindroma.");
+}
